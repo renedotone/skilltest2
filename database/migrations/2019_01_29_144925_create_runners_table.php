d2db0d332ff5881bcf4b81557c601130725092d4
renedotone <rmspijker@gmail.com>
2019-01-29 16:47:40 +0100

    added runners

---
+++ database/migrations/2019_01_29_144925_create_runners_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRunnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('runners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('naam');
+            $table->string('email');
+            $table->unsignedInteger('afstand');
+            $table->unsignedInteger('aantal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('runners');
+    }
+}
